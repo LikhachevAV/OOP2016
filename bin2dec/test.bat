fd70e0bc2d7cd21b4a09a80886f44f9070a5fa08
YolloPukki123 <temik_l@mail.ru>
2016-10-06 21:57:27 +0400

    Добавил логику для прохождения тестов на пустой аргумент и некорректную длину аргумета программы.

--- bin2dec/test.bat
+++ bin2dec/test.bat
@@ -1,24 +1,28 @@
 set PROGRAM="%~1"
 
 rem проверяем запуск без параметров
+echo run program without arguments:
 %PROGRAM% > %TEMP%\output.txt
 if NOT ERRORLEVEL 1 goto err
 fc.exe out_files\parameters_count_error.txt %TEMP%\output.txt
 if ERRORLEVEL 1 goto err
 
 rem проверка запуска с числом, длиной 33 в качестве параметра
+echo run program with argument length owerflow:
 %PROGRAM% 111111111111111111111111111111111 > %TEMP%\output.txt
 if NOT ERRORLEVEL 1 goto err
 fc.exe out_files\number_length_owerflow.txt %TEMP%\output.txt
 if ERRORLEVEL 1 goto err
 
 rem проверка запуска не с двоичным числом в качестве параметра
+echo run program with string as argument:
 %PROGRAM% 11a > %TEMP%\output.txt
 if NOT ERRORLEVEL 1 goto err
 fc.exe out_files\argument_type_error %TEMP%\output.txt
 if ERRORLEVEL 1 goto err
 
 rem проверка запуска не с двоичным числом в качестве параметра
+echo run program with decimal number as argument:
 %PROGRAM% 211 > %TEMP%\output.txt
 if NOT ERRORLEVEL 1 goto err
 fc.exe out_files\argument_type_error %TEMP%\output.txt
@@ -42,41 +46,6 @@ rem проверка правой крайней границы (с числом 11111111111111111111111111111110 в
 if ERRORLEVEL 1 goto err
 fc.exe out_files\ULONG_MAX.txt %TEMP%\output.txt
 if ERRORLEVEL 1 goto err
-
-
-goto success
-
-rem проверка запуска с разными однострочными файлами в качестве параметров
-%PROGRAM% one_line.txt one_line_different_line_1.txt > %TEMP%\output.txt
-if NOT ERRORLEVEL 1 goto err
-fc.exe out_files\defferent_files_line_1 %TEMP%\output.txt
-if ERRORLEVEL 1 goto err
-
-rem проверка запуска с одинаковыми многосторчными файлами в качестве параметров
-%PROGRAM% multiline.txt multiline_equals.txt > %TEMP%\output.txt
-if ERRORLEVEL 1 goto err
-fc.exe out_files\equal_files.txt %TEMP%\output.txt
-if ERRORLEVEL 1 goto err
-
-rem проверка запуска с разыми многосторчными файлами в качестве параметров с разницей в 3й строке
-%PROGRAM% multiline.txt multiline_different_line_3.txt > %TEMP%\output.txt
-if NOT ERRORLEVEL 1 goto err
-fc.exe out_files\defferent_files_line_3 %TEMP%\output.txt
-if ERRORLEVEL 1 goto err
-
-rem проверка запуска с файлами разной длины в качестве параметров с разницей во 2й строке
-%PROGRAM% multiline.txt multiline_different_size_line2.txt > %TEMP%\output.txt
-if NOT ERRORLEVEL 1 goto err
-fc.exe out_files\defferent_files_line_2 %TEMP%\output.txt
-if ERRORLEVEL 1 goto err
-
-rem проверка запуска с файлами разной длины в качестве параметров с разницей во 2й строке
-%PROGRAM% multiline_different_size_line2.txt multiline.txt > %TEMP%\output.txt
-if NOT ERRORLEVEL 1 goto err
-fc.exe out_files\defferent_files_line_2 %TEMP%\output.txt
-if ERRORLEVEL 1 goto err
-
-:success
 
 echo Program testing succeeded
 exit 0
