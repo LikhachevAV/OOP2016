f7ba15266d8cacdbad984b3daf73fbbf79ff40b4
YolloPukki123 <temik_l@mail.ru>
2016-10-07 00:56:14 +0400

    Bin2dec выделил в отдельную функцию.

--- bin2dec/main.cpp
+++ bin2dec/main.cpp
@@ -4,51 +4,55 @@
 using namespace std;
 
 int ERROR_STATE = -1;
+int MAX_SIZE_LENGTH = 32;
 
-int Bin2dec(string const &binString, unsigned long int &result, string &errorMessage)
+int Bin2dec(string const & binString, unsigned long int & result, string & errorMessage)
 {
+	auto inStrSize = binString.size();
+	if (inStrSize > MAX_SIZE_LENGTH)
+	{
+		errorMessage = errorMessage.append("Invalid arguments length!\nMax numbers length must be ")
+			.append(to_string(MAX_SIZE_LENGTH)).append(", but found ").append(to_string(inStrSize));
+		return ERROR_STATE;
+	}
 
+	result = 0;
+	unsigned long int multiplier = 1;
+
+	for (int i = inStrSize - 1; i >= 0; --i)
+	{
+		if (binString[i] == '1')
+		{
+			result += multiplier;
+		}
+		else if (binString[i] != '0')
+		{
+			errorMessage = errorMessage.append("Invalid argument type!\nUsage: bin2dec.exe <binary number>");
+			return ERROR_STATE;
+		}
+
+		multiplier = multiplier << 1;
+	}
 	return 0;
 }
 
 int main(int argc, char* argv[])
 {
-	string errorMessage = "";
-	int MAX_SIZE_LENGTH = 32;
 	if (argc != 2)
 	{
 		cout << "Invalid arguments count!" << endl
-			<< "Usage: bin2dec.exe <binary number>" <<endl;
+			<< "Usage: bin2dec.exe <binary number>" << endl;
 
 		return 1;
 	}
 
-	string inStr = argv[1];
-	auto inStrSize = inStr.size();
-	if (inStrSize > MAX_SIZE_LENGTH)
+	unsigned long int result;
+	string errorMessage = "";
+
+	if (Bin2dec(argv[1], result, errorMessage) == ERROR_STATE)
 	{
-		cout << "Invalid arguments length!" << endl
-			<< "Max numbers length must be "<< MAX_SIZE_LENGTH << ", but found " << inStrSize << endl;
+		cout << errorMessage << endl;
 		return 1;
-	}
-
-	unsigned long int result = 0;
-	unsigned long int multiplier = 1;
-
-	for (int i = inStrSize - 1; i >= 0 ; --i)
-	{
-		if (inStr[i] == '1')
-		{
-			result += multiplier;
-		}
-		else if (inStr[i] != '0')
-		{
-			cout << "Invalid argument type!" << endl
-				<< "Usage: bin2dec.exe <binary number>" << endl;
-			return 1;
-		}
-		
-		multiplier = multiplier << 1;
 	}
 
 	cout << result << endl;
