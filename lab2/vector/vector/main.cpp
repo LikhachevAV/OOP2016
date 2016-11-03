61679e191a33f6d8549e60bb1221ac53d932328e
Артем Лихачев <artem.lihachev@travelline.ru>
2016-11-03 15:00:50 +0300

    Добавил проектт для 2го задания

---
+++ lab2/vector/vector/main.cpp
@@ -0,0 +1,24 @@
+#include "stdafx.h"
+#include <iomanip>
+#include <iostream>
+#include <fstream>
+#include <string>
+#include "VectorProcessor.h"
+#include <iterator>
+
+using namespace std;
+
+void PrintVector(vector<double> numbers)
+{
+	copy(numbers.begin(), numbers.end(), ostream_iterator<double>(cout, " "));
+}
+
+int main()
+{
+	vector <double> numbers(istream_iterator<double>(cin), (istream_iterator<double>()));
+	ProcessVector(numbers);
+	SortVector(numbers);
+	PrintVector(numbers);
+	cout << endl;
+	return 0;
+}
