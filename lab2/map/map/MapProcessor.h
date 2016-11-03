22cad7c5acf51d2b60c6327ce1d251788d8ac464
YolloPukki123 <temik_l@mail.ru>
2016-11-03 21:11:17 +0300

    Удалил неиспользуемые методы

--- lab2/map/map/MapProcessor.h
+++ lab2/map/map/MapProcessor.h
@@ -1,7 +1,4 @@
 ﻿#pragma once
 #include <map>
 
-double GetVectorsMinVal(std::vector<double> & numbers);
-void SortVector(std::vector<double> & numbers);
-void ProcessVector(std::vector<double> & numbers);
 void AddWord(std::map<std::string, int> & words, const std::string & word);
