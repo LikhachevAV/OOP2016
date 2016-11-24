0e73d413beaa29176aea8a949f42870e177f9a17
YolloPukki123 <temik_l@mail.ru>
2016-11-24 22:42:43 +0300

    Добавил каркас проекта car

---
+++ lab3/car/car/CCar.cpp
@@ -0,0 +1,35 @@
+﻿#include "stdafx.h"
+#include "CCar.h"
+#pragma warning (push, 3)
+#include <boost/phoenix.hpp>
+#pragma warning (pop)
+
+using namespace std;
+using namespace std::placeholders;
+using namespace boost::phoenix::placeholders;
+using namespace boost::phoenix;
+
+void CCar::Info(std::ostream & stream)const
+{
+	
+}
+
+bool CCar::EngineOn()
+{
+	return true;
+}
+
+bool CCar::EngineOff()
+{
+	return true;
+}
+
+bool CCar::SetGear(int gear)
+{
+	return true;
+}
+
+bool CCar::SetSpeed(int speed)
+{
+	return true;
+}
---
+++ lab3/car/car/CCar.h
@@ -0,0 +1,17 @@
+﻿#pragma once
+#include <iostream>
+
+class CCar
+{
+public:
+	void Info(std::ostream & stream)const;
+	bool EngineOn();
+	bool EngineOff();
+	bool SetGear(int gear);
+	bool SetSpeed(int speed);
+
+private:
+	bool m_isEngineOn = false;
+	int m_currentGear = 0;
+	unsigned m_currentSpeed = 0;
+};
