2f2e79c10caec86c98fc26de6b23a1a043e1a3bc
YolloPukki123 <temik_l@mail.ru>
2016-12-02 00:16:46 +0300

    Добавил тест can_not_turn_off_when_speed_is_not_0

--- lab3/car/car_tests/CarTests.cpp
+++ lab3/car/car_tests/CarTests.cpp
@@ -36,4 +36,28 @@ BOOST_FIXTURE_TEST_SUITE(Car, CarFixture)
 		}
 	BOOST_AUTO_TEST_SUITE_END()
 
+	struct TurnedOnEngineCar : CarFixture
+	{
+		TurnedOnEngineCar()
+		{
+			car.EngineOn();
+		}
+	};
+
+	BOOST_FIXTURE_TEST_SUITE(CarFixture, TurnedOnEngineCar)
+		BOOST_AUTO_TEST_CASE(can_turn_off_turned_on_engine)
+		{
+			BOOST_CHECK(car.EngineOff());
+			BOOST_CHECK(!car.IsEngineOn());
+		}
+
+		BOOST_AUTO_TEST_CASE(can_not_turn_off_when_speed_is_not_0)
+		{
+			BOOST_CHECK(car.SetGear(1));
+			BOOST_CHECK(car.SetSpeed(30));
+			BOOST_CHECK(!car.EngineOff());
+			BOOST_CHECK(car.IsEngineOn());
+		}
+	BOOST_AUTO_TEST_SUITE_END()
+
 BOOST_AUTO_TEST_SUITE_END()
