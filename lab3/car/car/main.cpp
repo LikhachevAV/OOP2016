6f514a66935e82dc13022182a3c35ecad579a5a6
YolloPukki123 <temik_l@mail.ru>
2016-12-21 01:02:31 +0300

    добавил функцию ReadCommand

--- lab3/car/car/main.cpp
+++ lab3/car/car/main.cpp
@@ -1,9 +1,35 @@
-#include "stdafx.h"
+﻿#include "stdafx.h"
 #include "CCar.h"
 
 using namespace std;
+struct Command
+{
+	string command;
+	int value = 0;
+};
+
+bool ReadCommand(istream & in, Command & command)
+{
+	string commandStr;
+	in >> commandStr;
+	if (!in)
+	{
+		return false;
+	}
+	return true;
+}
 
 int main()
 {
+	Command command;
+	while (cin.eof())
+	{
+		if (!ReadCommand(cin, command))
+		{
+			cout << "Command reading error!" << endl
+				<< "possible commands: "
+				<< "Info, EngineOn, EngineOff, SetGear<gear>, SetSpeed<gear>" << endl;
+		}
+	}
 	return 0;
 }
