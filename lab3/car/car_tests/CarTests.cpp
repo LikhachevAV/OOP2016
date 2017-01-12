64b3ea8cfeaa7d425e0a3515c82bcbb899b89260
YolloPukki123 <temik_l@mail.ru>
2017-01-13 00:10:36 +0300

    Добавил проверку на переключение на нейтраль с любой передачи

--- lab3/car/car_tests/CarTests.cpp
+++ lab3/car/car_tests/CarTests.cpp
@@ -196,6 +196,12 @@ BOOST_AUTO_TEST_CASE(can_not_set_speed_more_than_30)
 	CarStatesCheck(car, true, 1, 30, Direction::forward,
 		GetSetSpeedError(1));
 }
+
+BOOST_AUTO_TEST_CASE(can_set_neutral_gear)
+{
+	BOOST_CHECK(car.SetGear(0));
+	CarStatesCheck(car, true, 0, 30, Direction::forward, blankString);
+}
 BOOST_AUTO_TEST_SUITE_END()
 //#########################
 
@@ -205,6 +211,11 @@ BOOST_FIXTURE_TEST_SUITE(When_car_on_second_gear_and_its_speed_50, InSecondGearW
 		BOOST_CHECK(!car.SetSpeed(51));
 		CarStatesCheck(car, true, 2, 50, Direction::forward,
 			GetSetSpeedError(2));
+	}
+	BOOST_AUTO_TEST_CASE(can_set_neutral_gear)
+	{
+		BOOST_CHECK(car.SetGear(0));
+		CarStatesCheck(car, true, 0, 50, Direction::forward, blankString);
 	}
 BOOST_AUTO_TEST_SUITE_END()
 //#########################
@@ -216,6 +227,11 @@ BOOST_FIXTURE_TEST_SUITE(When_car_on_third_gear_and_its_speed_60, InThirdGearWit
 		CarStatesCheck(car, true, 3, 60, Direction::forward,
 			GetSetSpeedError(3));
 	}
+	BOOST_AUTO_TEST_CASE(can_set_neutral_gear)
+	{
+		BOOST_CHECK(car.SetGear(0));
+		CarStatesCheck(car, true, 0, 60, Direction::forward, blankString);
+	}
 BOOST_AUTO_TEST_SUITE_END()
 //#########################
 
@@ -226,10 +242,20 @@ BOOST_FIXTURE_TEST_SUITE(When_car_on_fourth_gear_and_its_speed_90, InFourthGearW
 		CarStatesCheck(car, true, 4, 90, Direction::forward,
 			GetSetSpeedError(4));
 	}
+	BOOST_AUTO_TEST_CASE(can_set_neutral_gear)
+	{
+		BOOST_CHECK(car.SetGear(0));
+		CarStatesCheck(car, true, 0, 90, Direction::forward, blankString);
+	}
 BOOST_AUTO_TEST_SUITE_END()
 //#########################
 
 BOOST_FIXTURE_TEST_SUITE(When_car_on_fifth_gear_and_its_speed_150, InFifthGearWithMaxSpeed150)
+	BOOST_AUTO_TEST_CASE(can_set_neutral_gear)
+	{
+		BOOST_CHECK(car.SetGear(0));
+		CarStatesCheck(car, true, 0, 150, Direction::forward, blankString);
+	}
 	BOOST_AUTO_TEST_CASE(can_set_speed_50)
 	{
 		BOOST_CHECK(car.SetSpeed(50));
@@ -247,13 +273,6 @@ BOOST_FIXTURE_TEST_SUITE(When_car_on_fifth_gear_and_its_speed_150, InFifthGearWi
 		BOOST_CHECK(!car.SetGear(6));
 		CarStatesCheck(car, true, 5, 150, Direction::forward,
 			GetSetGearError(6));
-	}
-
-	BOOST_AUTO_TEST_CASE(can_set_neutral_gear)
-	{
-		BOOST_CHECK(car.SetGear(0));
-		CarStatesCheck(car, true, 0, 150, Direction::forward,
-			blankString);
 	}
 BOOST_AUTO_TEST_SUITE_END()
 
