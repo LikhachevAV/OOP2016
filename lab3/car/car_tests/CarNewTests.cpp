7f317634545b50cc349adc8f3551b6bc4fa8ce30
Артем Лихачев <artem.lihachev@travelline.ru>
2017-01-12 16:46:48 +0300

    Проверку при выключенном двигателе разбил на разные кейсы по передачам

--- lab3/car/car_tests/CarNewTests.cpp
+++ lab3/car/car_tests/CarNewTests.cpp
@@ -3,26 +3,57 @@
 #include "CarNewTests.h"
 
 BOOST_FIXTURE_TEST_SUITE(When_car_engine_is_off, CarFixture)
+
+auto switchAndCheckGearAndSpeed = [&](CCar car, int gear, unsigned speed) {
+	BOOST_CHECK(!car.SetGear(gear));
+	CarStatesCheck(car, false, 0, 0, Direction::stop);
+	BOOST_CHECK(!car.SetSpeed(speed));
+	CarStatesCheck(car, false, 0, 0, Direction::stop);
+};
 		
-	BOOST_AUTO_TEST_CASE(can_not_switch_gears_and_set_speed)
+	BOOST_AUTO_TEST_CASE(can_not_switch_less_than_MIN_GEAR_gear)
+	{	
+		switchAndCheckGearAndSpeed(car, -2, 0);
+		switchAndCheckGearAndSpeed(car , -2, 151);
+	}
+	BOOST_AUTO_TEST_CASE(can_not_switch_reverse_gear)
 	{
-		auto switchAndCheckGearAndSpeed = [&] (int gear, unsigned speed) {
-			BOOST_CHECK(!car.SetGear(gear));
-			CarStatesCheck(car, false, 0, 0, Direction::stop);
-			BOOST_CHECK(!car.SetSpeed(speed));
-			CarStatesCheck(car, false, 0, 0, Direction::stop);
-		};
-		switchAndCheckGearAndSpeed(-2, 151);
-		switchAndCheckGearAndSpeed(-1, 20);
-		switchAndCheckGearAndSpeed(0, 0);
-		switchAndCheckGearAndSpeed(0, 150);
-		switchAndCheckGearAndSpeed(1, 30);
-		switchAndCheckGearAndSpeed(2, 50);
-		switchAndCheckGearAndSpeed(2, 51);
-		switchAndCheckGearAndSpeed(3, 30);
-		switchAndCheckGearAndSpeed(4, 90);
-		switchAndCheckGearAndSpeed(5, 150);
-		switchAndCheckGearAndSpeed(5, 49);
+		switchAndCheckGearAndSpeed(car, -1, 20);
+	}
+	BOOST_AUTO_TEST_CASE(can_not_switch_same_gear)
+	{
+		switchAndCheckGearAndSpeed(car, 0, 0);
+		switchAndCheckGearAndSpeed(car, 0, 150);
+		switchAndCheckGearAndSpeed(car, 0, 151);
+	}
+	BOOST_AUTO_TEST_CASE(can_not_switch_first_gear_and_speed_30)
+	{
+		switchAndCheckGearAndSpeed(car, 1, 30);
+	}
+	BOOST_AUTO_TEST_CASE(can_not_switch_second_gear_and_speeds_20_50_51)
+	{
+		switchAndCheckGearAndSpeed(car, 2, 20);
+		switchAndCheckGearAndSpeed(car, 2, 50);
+		switchAndCheckGearAndSpeed(car, 2, 51);
+	}
+	BOOST_AUTO_TEST_CASE(can_not_switch_third_gear_and_speeds_29_30)
+	{
+		switchAndCheckGearAndSpeed(car, 3, 29);
+		switchAndCheckGearAndSpeed(car, 3, 30);
+	}
+	BOOST_AUTO_TEST_CASE(can_not_switch_fourth_gear_and_speeds_90_91)
+	{
+		switchAndCheckGearAndSpeed(car, 4, 90);
+		switchAndCheckGearAndSpeed(car, 4, 91);
+	}
+	BOOST_AUTO_TEST_CASE(can_not_switch_fifth_gear_and_speeds_150)
+	{
+		switchAndCheckGearAndSpeed(car, 5, 150);
+	}
+	BOOST_AUTO_TEST_CASE(can_not_switch_more_than_MAX_GEAR_gear)
+	{
+		switchAndCheckGearAndSpeed(car, 6, 150);
+		switchAndCheckGearAndSpeed(car, 6, 0);
 	}
 
 BOOST_AUTO_TEST_SUITE_END()
