ce603c75be27304cca78e51b4646f051ddd49d9d
YolloPukki123 <temik_l@mail.ru>
2017-01-17 22:42:29 +0300

    Правки в методах согласно замечаниям

--- lab5/Vector3D/Vector3D/CVector3D.cpp
+++ lab5/Vector3D/Vector3D/CVector3D.cpp
@@ -6,16 +6,11 @@
 
 using namespace std;
 
-CVector3D::CVector3D()
+CVector3D::CVector3D(double x0, double y0, double z0):
+	x(x0),
+	y(y0),
+	z(z0)
 {
-	x = y = z = 0.0;
-}
-
-CVector3D::CVector3D(double x0, double y0, double z0)
-{
-	x = x0;
-	y = y0;
-	z = z0;
 }
 
 double CVector3D::GetLength()const
@@ -23,65 +18,62 @@ double CVector3D::GetLength()const
 	return sqrt(x * x + y * y + z * z);
 }
 
-CVector3D CVector3D::operator+ (CVector3D const & vector1) const
+const CVector3D CVector3D::Normalize()
 {
-	CVector3D result;
-	result.x = x + vector1.x;
-	result.y = y + vector1.y;
-	result.z = z + vector1.z;
-	return result;
+	double length = GetLength();
+	if (length != 0)
+	{
+		x /= length;
+		y /= length;
+		z /= length;
+	}
+	return *this;
 }
 
-CVector3D CVector3D::operator+ () const
+const CVector3D CVector3D::operator+ (CVector3D const & vector1) const
+{
+	return{ x + vector1.x, y + vector1.y, z + vector1.z };
+}
+
+const CVector3D CVector3D::operator+ () const
 {
 	return *this;
 }
 
-CVector3D CVector3D::operator- (CVector3D const & vector1) const
+const CVector3D CVector3D::operator- (CVector3D const & vector1) const
 {
-	CVector3D result;
-	result.x = x - vector1.x;
-	result.y = y - vector1.y;
-	result.z = z - vector1.z;
-	return result;
+	return{ x - vector1.x, y - vector1.y, z - vector1.z };
 }
 
-CVector3D CVector3D::CVector3D::operator- () const
+const CVector3D CVector3D::CVector3D::operator- () const
 {
-	CVector3D result;
-	return *this * -1;
+	return{ -x, -y, -z };
 }
 
 CVector3D operator* (CVector3D const & vector3d, double scalar)
 {
-	CVector3D result;
-	result.x = vector3d.x * scalar;
-	result.y = vector3d.y * scalar;
-	result.z = vector3d.z * scalar;
-	return result;
+	return{ vector3d.x * scalar, vector3d.y * scalar, vector3d.z * scalar };
 };
 
 CVector3D operator* (double scalar, CVector3D const & vector3d)
 {
-	CVector3D result;
-	result.x = scalar * vector3d.x;
-	result.y = scalar * vector3d.y;
-	result.z = scalar * vector3d.z;
-	return result;
+	return{ scalar * vector3d.x, scalar * vector3d.y, scalar * vector3d.z };
 };
 
-void CVector3D::operator+= (CVector3D const & vector1)
+CVector3D CVector3D::operator+= (CVector3D const & vector1)
 {
 	x += vector1.x;
 	y += vector1.y;
 	z += vector1.z;
+	return *this;
 };
 
-void CVector3D::operator-= (CVector3D const & vector1)
+CVector3D  CVector3D::operator-= (CVector3D const & vector1)
 {
 	x -= vector1.x;
 	y -= vector1.y;
 	z -= vector1.z;
+	return *this;
 };
 
 CVector3D CVector3D::operator/ (double scalar) const 
@@ -90,21 +82,18 @@ CVector3D CVector3D::operator/ (double scalar) const
 	{
 		throw invalid_argument("Divizion by zero error!");
 	}
-	CVector3D result;
-	result.x = x / scalar;
-	result.y = y / scalar;
-	result.z = z / scalar;
-	return result;
+	return{ x / scalar, y / scalar, z / scalar };
 }
 
-void CVector3D::operator*= (double scalar)
+CVector3D CVector3D::operator*= (double scalar)
 {
 	x *= scalar;
 	y *= scalar;
 	z *= scalar;
+	return *this;
 }
 
-void CVector3D::operator/= (double scalar)
+CVector3D CVector3D::operator/= (double scalar)
 {
 	if (scalar == 0)
 	{
@@ -113,6 +102,7 @@ void CVector3D::operator/= (double scalar)
 	x /= scalar;
 	y /= scalar;
 	z /= scalar;
+	return *this;
 }
 
 bool CVector3D::operator==(CVector3D const & vector1) const
@@ -124,7 +114,3 @@ bool CVector3D::operator!=(CVector3D const & vector1) const
 {
 	return (x != vector1.x) || (y != vector1.y) || (z != vector1.z);
 }
-
-/* TODO:
-Normalize()
-*/
--- lab5/Vector3D/Vector3D/CVector3D.h
+++ lab5/Vector3D/Vector3D/CVector3D.h
@@ -4,24 +4,27 @@ class CVector3D
 {
 public:
 
-	CVector3D();
+	CVector3D() = default;
 	CVector3D(double x0, double y0, double z0);
 
 	double GetLength()const;
-	void Normalize();
-	CVector3D operator+ (CVector3D const & vector1)  const;
-	CVector3D operator+ () const;
-	CVector3D operator- (CVector3D const & vector1) const;
-	CVector3D operator- () const;
-	friend CVector3D operator* (CVector3D const & vector3d, double scalar);
-	friend CVector3D operator* (double scalar, CVector3D const & vector3d);
-	void operator+= (CVector3D const & vector1);
-	void operator-= (CVector3D const & vector1);
+	const CVector3D Normalize();
+	const CVector3D operator+ (CVector3D const & vector1)  const;
+	const CVector3D operator+ () const;
+	const CVector3D operator- (CVector3D const & vector1) const;
+	const CVector3D operator- () const;
+	CVector3D operator+= (CVector3D const & vector1);
+	CVector3D operator-= (CVector3D const & vector1);
 	CVector3D operator/ (double scalar) const;
-	void operator*= (double scalar);
-	void operator/= (double scalar);
+	CVector3D operator*= (double scalar);
+	CVector3D operator/= (double scalar);
 	bool operator==(CVector3D const & vector1) const;
 	bool operator!=(CVector3D const & vector1) const;
 
-	double x, y, z;
+	double x = 0.0;
+	double y = 0.0;
+	double z = 0.0;
 };
+
+CVector3D operator* (CVector3D const & vector3d, double scalar);
+CVector3D operator* (double scalar, CVector3D const & vector3d);
