b2880809aa06a28c445fab841065ee4e5194787f
YolloPukki123 <temik_l@mail.ru>
2017-01-26 21:17:25 +0300

    Правка в CPoint после сбоя

--- lab4_new/Shapes/Shapes/CPoint.cpp
+++ lab4_new/Shapes/Shapes/CPoint.cpp
@@ -1,15 +1,16 @@
-#pragma once
 #include "stdafx.h"
+#include "CPoint.h"
 
-class CPoint
+CPoint::CPoint(double x0, double y0) :
+	x(x0),
+	y(y0)
 {
-public:
-	double x = 0;
-	double y = 0;
+}
 
-	CPoint(double x0, double y0);
-
-	CPoint() = default;
-
-	std::string ToString() const;
-};
+std::string CPoint::ToString() const
+{
+	std::stringstream strm;
+	strm << std::fixed << std::setprecision(1)
+		<< "(" << x << ", " << y << ")";
+	return strm.str();
+}
