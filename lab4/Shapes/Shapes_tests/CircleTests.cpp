69d8245227a3a350f6c7ec54da0584c98bf84f51
YolloPukki123 <temik_l@mail.ru>
2017-01-28 03:13:10 +0300

    Добавил тест для circle

--- lab4/Shapes/Shapes_tests/CircleTests.cpp
+++ lab4/Shapes/Shapes_tests/CircleTests.cpp
@@ -27,4 +27,22 @@ BOOST_AUTO_TEST_SUITE(Circle_constructor)
 		string circleOutStr = "circle (0.0, 0.0) 12.2 red white";
 		BOOST_CHECK_EQUAL(circle.ToString(), circleOutStr);
 	}
+
+	BOOST_AUTO_TEST_CASE(can_construct_circle_with_center_in_not_0_0_point)
+	{
+		CPoint center(-3, 2.5);
+		double diameter = 10;
+		string outlineColor = "purple";
+		string fillColor = "blue";
+		CCircle circle(center, diameter, outlineColor, fillColor);
+		BOOST_CHECK(circle.GetCenter() == CPoint(-3, 2.5));
+		BOOST_CHECK_EQUAL(circle.GetOutlineColor(), outlineColor);
+		BOOST_CHECK_EQUAL(circle.GetFillColor(), fillColor);
+		auto perimeter = M_PI * diameter;
+		BOOST_CHECK_EQUAL(circle.GetPerimeter(), perimeter);
+		auto area = GetCircleArea(diameter);
+		BOOST_CHECK_EQUAL(circle.GetArea(), area);
+		string circleOutStr = "circle (-3.0, 2.5) 10.0 purple blue";
+		BOOST_CHECK_EQUAL(circle.ToString(), circleOutStr);
+	}
 BOOST_AUTO_TEST_SUITE_END()
