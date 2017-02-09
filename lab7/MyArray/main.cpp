cab6c7ac17dc06b526d35e6b01fdfe8681280a3b
YolloPukki123 <temik_l@mail.ru>
2017-02-09 22:48:58 +0300

    В main.cpp() добавил массив из double

--- lab7/MyArray/main.cpp
+++ lab7/MyArray/main.cpp
@@ -11,14 +11,36 @@ void main(void)
 	stringArray.Append("say");
 	stringArray.Append("Hello");
 	stringArray.Append("!");
-	cout << "Print array, using begin, end iterators: " << endl;
+
+	cout << "Print string array, using begin, end iterators: " << endl;
 	for (auto it = stringArray.begin(); it != stringArray.end(); it++)
 	{
 		cout << *it << " ";
 	}
 	cout << endl << endl;
-	cout << "Print reverse array, using rbegin, rend iterators: " << endl;
+
+	cout << "Print reverse string array, using rbegin, rend iterators: " << endl;
 	for (auto it = stringArray.rbegin(); it != stringArray.rend(); it++)
+	{
+		cout << *it << " ";
+	}
+	cout << endl << endl;
+
+	CMyArray<double> doubleArray;
+	for (double d = 2.2; d < 10; d += 1.1)
+	{
+		doubleArray.Append(d);
+	}
+
+	cout << std::fixed << std::setprecision(1) <<"Print double array, using begin, end iterators: " << endl;
+	for (auto it = doubleArray.begin(); it != doubleArray.end(); it++)
+	{
+		cout << *it << " ";
+	}
+	cout << endl << endl;
+
+	cout << "Print reverse double array, using rbegin, rend iterators: " << endl;
+	for (auto it = doubleArray.rbegin(); it != doubleArray.rend(); it++)
 	{
 		cout << *it << " ";
 	}
