22d2c838db56318784a2db89fcc92468cc98e582
YolloPukki123 <temik_l@mail.ru>
2017-02-11 10:57:49 +0300

    Добавил блок try-catch в операторах присваивания

--- lab6/Student/Student/Student.cpp
+++ lab6/Student/Student/Student.cpp
@@ -20,11 +20,17 @@ CStudent::CStudent(std::string const & surname, std::string const & name,
 		throw std::out_of_range("Age value must be between" + std::to_string(MIN_AGE)
 			+ "and" + std::to_string(MAX_AGE));
 	}
-
-	m_name = name;
-	m_surname = surname;
-	m_patronymic = patronymic;
-	m_age = age;
+	try
+	{
+		m_name = name;
+		m_surname = surname;
+		m_patronymic = patronymic;
+		m_age = age;
+	}
+	catch (std::exception)
+	{
+		throw;
+	}
 }
 
 
@@ -71,24 +77,25 @@ void CStudent::SetAge(int age)
 
 void CStudent::Rename(std::string const & name, std::string const & surname, std::string const & patronymic)
 {
-	if ((name.length() > 0) && (name.compare(" ") != 0) &&
-		(surname.length() > 0) && (surname.compare(" ") != 0))
-	{
-		m_name = name;
-		m_surname = surname;
-	}
-	else
+	if ((name.length() == 0) || (StringContainOnlySpaces(name)) ||
+		(surname.length() == 0) || (StringContainOnlySpaces(surname)))
 	{
 		throw std::invalid_argument("Names and surnames values must be non empty and non space value");
 	}
 
-	if ((name.length() > 0) && (patronymic.compare(" ") == 0))
+	if ((patronymic.length() > 0) && (StringContainOnlySpaces(patronymic)))
 	{
 		throw std::invalid_argument("Patronymic value must non space value");
 	}
-	else
+	try
 	{
+		m_name = name;
+		m_surname = surname;
 		m_patronymic = patronymic;
+	}
+	catch (std::exception)
+	{
+		throw;
 	}
 }
 
