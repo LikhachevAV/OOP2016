142bfc2f9ea8ac602d12deba52da9474c53f0d51
YolloPukki123 <temik_l@mail.ru>
2017-02-16 21:02:27 +0300

    Добавил вспомогательные функции поиска площади и периметра в тестовом проекте

--- lab6/Triangle/Triangle_tests/TriangleTests.cpp
+++ lab6/Triangle/Triangle_tests/TriangleTests.cpp
@@ -3,6 +3,19 @@
 
 using namespace std;
 
+auto GetTrianglePerimeter(CTriangle const & triangle)
+{
+	return triangle.GetSide1() + triangle.GetSide2() + triangle.GetSide3();
+}
+
+auto GetTriangleArea(CTriangle const & triangle)
+{
+	auto p = GetTrianglePerimeter(triangle);
+	auto area = sqrt(p * (p - triangle.GetSide1()) * 
+		(p - triangle.GetSide2()) * (p - triangle.GetSide3()));
+	return area;
+}
+
 BOOST_AUTO_TEST_SUITE(Can_not_create_triangle)
 
 	BOOST_AUTO_TEST_CASE(with_negative_side1)
@@ -44,5 +57,6 @@ BOOST_AUTO_TEST_SUITE(DegenerateTriangle)
 		BOOST_CHECK_EQUAL(degenerateTriangle.GetSide2(), 3.0);
 		BOOST_CHECK_EQUAL(degenerateTriangle.GetSide3(), 4.0);
 		BOOST_CHECK_EQUAL(degenerateTriangle.GetArea(), 0);
+		BOOST_CHECK_EQUAL(degenerateTriangle.GetPerimeter(), GetTrianglePerimeter(degenerateTriangle));
 	}
 BOOST_AUTO_TEST_SUITE_END()
