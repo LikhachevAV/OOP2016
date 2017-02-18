55fced557e880ab592f13596bb456f2a0d72561a
YolloPukki123 <temik_l@mail.ru>
2017-02-18 16:41:30 +0300

    Добавил каркас StringList

---
+++ lab6/StringList/StringList/stdafx.h
@@ -0,0 +1,21 @@
+// stdafx.h : include file for standard system include files,
+// or project specific include files that are used frequently, but
+// are changed infrequently
+//
+
+#pragma once
+
+#define _USE_MATH_DEFINES  
+#include <cmath> 
+#include "targetver.h"
+#include <tchar.h>
+#include <iostream>
+#include <iomanip>
+#include <sstream>
+#include <string>
+#include <math.h>
+#include <vector>
+#include <boost/shared_ptr.hpp>
+
+
+// TODO: reference additional headers your program requires here
